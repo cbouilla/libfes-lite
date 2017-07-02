aff13ba901d60837d25882c6a0d0e9243d5ac734
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2017-07-02 08:01:49 +0200

    reduced unrolling in plain C code

--- src/generic/enum_1x32.c
+++ src/generic/enum_1x32.c
@@ -5,7 +5,7 @@
 
 #include "monomials.h"
 
-
+#define L 8
 
 struct solution_t {
   uint32_t x;
@@ -15,7 +15,7 @@ struct context_t {
 	int n;
 	const uint32_t * const F_start;
 	uint32_t * F;
-	struct solution_t buffer[512 + 32];
+	struct solution_t buffer[(1 << L) + 32];
 	size_t buffer_size;
 	uint32_t *solutions;
 	size_t max_solutions;
@@ -341,263 +341,6 @@ static inline void unrolled_chunk(struct context_t *context, int alpha, uint32_t
 	STEP_2(context, 1, 5, i + 253);
 	STEP_2(context, 2, 6, i + 254);
 	STEP_2(context, 1, 3, i + 255);
-	STEP_2(context, 37, 9 + alpha, i + 256);
-	STEP_2(context, 1, 38, i + 257);
-	STEP_2(context, 2, 39, i + 258);
-	STEP_2(context, 1, 3, i + 259);
-	STEP_2(context, 4, 40, i + 260);
-	STEP_2(context, 1, 5, i + 261);
-	STEP_2(context, 2, 6, i + 262);
-	STEP_2(context, 1, 3, i + 263);
-	STEP_2(context, 7, 41, i + 264);
-	STEP_2(context, 1, 8, i + 265);
-	STEP_2(context, 2, 9, i + 266);
-	STEP_2(context, 1, 3, i + 267);
-	STEP_2(context, 4, 10, i + 268);
-	STEP_2(context, 1, 5, i + 269);
-	STEP_2(context, 2, 6, i + 270);
-	STEP_2(context, 1, 3, i + 271);
-	STEP_2(context, 11, 42, i + 272);
-	STEP_2(context, 1, 12, i + 273);
-	STEP_2(context, 2, 13, i + 274);
-	STEP_2(context, 1, 3, i + 275);
-	STEP_2(context, 4, 14, i + 276);
-	STEP_2(context, 1, 5, i + 277);
-	STEP_2(context, 2, 6, i + 278);
-	STEP_2(context, 1, 3, i + 279);
-	STEP_2(context, 7, 15, i + 280);
-	STEP_2(context, 1, 8, i + 281);
-	STEP_2(context, 2, 9, i + 282);
-	STEP_2(context, 1, 3, i + 283);
-	STEP_2(context, 4, 10, i + 284);
-	STEP_2(context, 1, 5, i + 285);
-	STEP_2(context, 2, 6, i + 286);
-	STEP_2(context, 1, 3, i + 287);
-	STEP_2(context, 16, 43, i + 288);
-	STEP_2(context, 1, 17, i + 289);
-	STEP_2(context, 2, 18, i + 290);
-	STEP_2(context, 1, 3, i + 291);
-	STEP_2(context, 4, 19, i + 292);
-	STEP_2(context, 1, 5, i + 293);
-	STEP_2(context, 2, 6, i + 294);
-	STEP_2(context, 1, 3, i + 295);
-	STEP_2(context, 7, 20, i + 296);
-	STEP_2(context, 1, 8, i + 297);
-	STEP_2(context, 2, 9, i + 298);
-	STEP_2(context, 1, 3, i + 299);
-	STEP_2(context, 4, 10, i + 300);
-	STEP_2(context, 1, 5, i + 301);
-	STEP_2(context, 2, 6, i + 302);
-	STEP_2(context, 1, 3, i + 303);
-	STEP_2(context, 11, 21, i + 304);
-	STEP_2(context, 1, 12, i + 305);
-	STEP_2(context, 2, 13, i + 306);
-	STEP_2(context, 1, 3, i + 307);
-	STEP_2(context, 4, 14, i + 308);
-	STEP_2(context, 1, 5, i + 309);
-	STEP_2(context, 2, 6, i + 310);
-	STEP_2(context, 1, 3, i + 311);
-	STEP_2(context, 7, 15, i + 312);
-	STEP_2(context, 1, 8, i + 313);
-	STEP_2(context, 2, 9, i + 314);
-	STEP_2(context, 1, 3, i + 315);
-	STEP_2(context, 4, 10, i + 316);
-	STEP_2(context, 1, 5, i + 317);
-	STEP_2(context, 2, 6, i + 318);
-	STEP_2(context, 1, 3, i + 319);
-	STEP_2(context, 22, 44, i + 320);
-	STEP_2(context, 1, 23, i + 321);
-	STEP_2(context, 2, 24, i + 322);
-	STEP_2(context, 1, 3, i + 323);
-	STEP_2(context, 4, 25, i + 324);
-	STEP_2(context, 1, 5, i + 325);
-	STEP_2(context, 2, 6, i + 326);
-	STEP_2(context, 1, 3, i + 327);
-	STEP_2(context, 7, 26, i + 328);
-	STEP_2(context, 1, 8, i + 329);
-	STEP_2(context, 2, 9, i + 330);
-	STEP_2(context, 1, 3, i + 331);
-	STEP_2(context, 4, 10, i + 332);
-	STEP_2(context, 1, 5, i + 333);
-	STEP_2(context, 2, 6, i + 334);
-	STEP_2(context, 1, 3, i + 335);
-	STEP_2(context, 11, 27, i + 336);
-	STEP_2(context, 1, 12, i + 337);
-	STEP_2(context, 2, 13, i + 338);
-	STEP_2(context, 1, 3, i + 339);
-	STEP_2(context, 4, 14, i + 340);
-	STEP_2(context, 1, 5, i + 341);
-	STEP_2(context, 2, 6, i + 342);
-	STEP_2(context, 1, 3, i + 343);
-	STEP_2(context, 7, 15, i + 344);
-	STEP_2(context, 1, 8, i + 345);
-	STEP_2(context, 2, 9, i + 346);
-	STEP_2(context, 1, 3, i + 347);
-	STEP_2(context, 4, 10, i + 348);
-	STEP_2(context, 1, 5, i + 349);
-	STEP_2(context, 2, 6, i + 350);
-	STEP_2(context, 1, 3, i + 351);
-	STEP_2(context, 16, 28, i + 352);
-	STEP_2(context, 1, 17, i + 353);
-	STEP_2(context, 2, 18, i + 354);
-	STEP_2(context, 1, 3, i + 355);
-	STEP_2(context, 4, 19, i + 356);
-	STEP_2(context, 1, 5, i + 357);
-	STEP_2(context, 2, 6, i + 358);
-	STEP_2(context, 1, 3, i + 359);
-	STEP_2(context, 7, 20, i + 360);
-	STEP_2(context, 1, 8, i + 361);
-	STEP_2(context, 2, 9, i + 362);
-	STEP_2(context, 1, 3, i + 363);
-	STEP_2(context, 4, 10, i + 364);
-	STEP_2(context, 1, 5, i + 365);
-	STEP_2(context, 2, 6, i + 366);
-	STEP_2(context, 1, 3, i + 367);
-	STEP_2(context, 11, 21, i + 368);
-	STEP_2(context, 1, 12, i + 369);
-	STEP_2(context, 2, 13, i + 370);
-	STEP_2(context, 1, 3, i + 371);
-	STEP_2(context, 4, 14, i + 372);
-	STEP_2(context, 1, 5, i + 373);
-	STEP_2(context, 2, 6, i + 374);
-	STEP_2(context, 1, 3, i + 375);
-	STEP_2(context, 7, 15, i + 376);
-	STEP_2(context, 1, 8, i + 377);
-	STEP_2(context, 2, 9, i + 378);
-	STEP_2(context, 1, 3, i + 379);
-	STEP_2(context, 4, 10, i + 380);
-	STEP_2(context, 1, 5, i + 381);
-	STEP_2(context, 2, 6, i + 382);
-	STEP_2(context, 1, 3, i + 383);
-	STEP_2(context, 29, 45, i + 384);
-	STEP_2(context, 1, 30, i + 385);
-	STEP_2(context, 2, 31, i + 386);
-	STEP_2(context, 1, 3, i + 387);
-	STEP_2(context, 4, 32, i + 388);
-	STEP_2(context, 1, 5, i + 389);
-	STEP_2(context, 2, 6, i + 390);
-	STEP_2(context, 1, 3, i + 391);
-	STEP_2(context, 7, 33, i + 392);
-	STEP_2(context, 1, 8, i + 393);
-	STEP_2(context, 2, 9, i + 394);
-	STEP_2(context, 1, 3, i + 395);
-	STEP_2(context, 4, 10, i + 396);
-	STEP_2(context, 1, 5, i + 397);
-	STEP_2(context, 2, 6, i + 398);
-	STEP_2(context, 1, 3, i + 399);
-	STEP_2(context, 11, 34, i + 400);
-	STEP_2(context, 1, 12, i + 401);
-	STEP_2(context, 2, 13, i + 402);
-	STEP_2(context, 1, 3, i + 403);
-	STEP_2(context, 4, 14, i + 404);
-	STEP_2(context, 1, 5, i + 405);
-	STEP_2(context, 2, 6, i + 406);
-	STEP_2(context, 1, 3, i + 407);
-	STEP_2(context, 7, 15, i + 408);
-	STEP_2(context, 1, 8, i + 409);
-	STEP_2(context, 2, 9, i + 410);
-	STEP_2(context, 1, 3, i + 411);
-	STEP_2(context, 4, 10, i + 412);
-	STEP_2(context, 1, 5, i + 413);
-	STEP_2(context, 2, 6, i + 414);
-	STEP_2(context, 1, 3, i + 415);
-	STEP_2(context, 16, 35, i + 416);
-	STEP_2(context, 1, 17, i + 417);
-	STEP_2(context, 2, 18, i + 418);
-	STEP_2(context, 1, 3, i + 419);
-	STEP_2(context, 4, 19, i + 420);
-	STEP_2(context, 1, 5, i + 421);
-	STEP_2(context, 2, 6, i + 422);
-	STEP_2(context, 1, 3, i + 423);
-	STEP_2(context, 7, 20, i + 424);
-	STEP_2(context, 1, 8, i + 425);
-	STEP_2(context, 2, 9, i + 426);
-	STEP_2(context, 1, 3, i + 427);
-	STEP_2(context, 4, 10, i + 428);
-	STEP_2(context, 1, 5, i + 429);
-	STEP_2(context, 2, 6, i + 430);
-	STEP_2(context, 1, 3, i + 431);
-	STEP_2(context, 11, 21, i + 432);
-	STEP_2(context, 1, 12, i + 433);
-	STEP_2(context, 2, 13, i + 434);
-	STEP_2(context, 1, 3, i + 435);
-	STEP_2(context, 4, 14, i + 436);
-	STEP_2(context, 1, 5, i + 437);
-	STEP_2(context, 2, 6, i + 438);
-	STEP_2(context, 1, 3, i + 439);
-	STEP_2(context, 7, 15, i + 440);
-	STEP_2(context, 1, 8, i + 441);
-	STEP_2(context, 2, 9, i + 442);
-	STEP_2(context, 1, 3, i + 443);
-	STEP_2(context, 4, 10, i + 444);
-	STEP_2(context, 1, 5, i + 445);
-	STEP_2(context, 2, 6, i + 446);
-	STEP_2(context, 1, 3, i + 447);
-	STEP_2(context, 22, 36, i + 448);
-	STEP_2(context, 1, 23, i + 449);
-	STEP_2(context, 2, 24, i + 450);
-	STEP_2(context, 1, 3, i + 451);
-	STEP_2(context, 4, 25, i + 452);
-	STEP_2(context, 1, 5, i + 453);
-	STEP_2(context, 2, 6, i + 454);
-	STEP_2(context, 1, 3, i + 455);
-	STEP_2(context, 7, 26, i + 456);
-	STEP_2(context, 1, 8, i + 457);
-	STEP_2(context, 2, 9, i + 458);
-	STEP_2(context, 1, 3, i + 459);
-	STEP_2(context, 4, 10, i + 460);
-	STEP_2(context, 1, 5, i + 461);
-	STEP_2(context, 2, 6, i + 462);
-	STEP_2(context, 1, 3, i + 463);
-	STEP_2(context, 11, 27, i + 464);
-	STEP_2(context, 1, 12, i + 465);
-	STEP_2(context, 2, 13, i + 466);
-	STEP_2(context, 1, 3, i + 467);
-	STEP_2(context, 4, 14, i + 468);
-	STEP_2(context, 1, 5, i + 469);
-	STEP_2(context, 2, 6, i + 470);
-	STEP_2(context, 1, 3, i + 471);
-	STEP_2(context, 7, 15, i + 472);
-	STEP_2(context, 1, 8, i + 473);
-	STEP_2(context, 2, 9, i + 474);
-	STEP_2(context, 1, 3, i + 475);
-	STEP_2(context, 4, 10, i + 476);
-	STEP_2(context, 1, 5, i + 477);
-	STEP_2(context, 2, 6, i + 478);
-	STEP_2(context, 1, 3, i + 479);
-	STEP_2(context, 16, 28, i + 480);
-	STEP_2(context, 1, 17, i + 481);
-	STEP_2(context, 2, 18, i + 482);
-	STEP_2(context, 1, 3, i + 483);
-	STEP_2(context, 4, 19, i + 484);
-	STEP_2(context, 1, 5, i + 485);
-	STEP_2(context, 2, 6, i + 486);
-	STEP_2(context, 1, 3, i + 487);
-	STEP_2(context, 7, 20, i + 488);
-	STEP_2(context, 1, 8, i + 489);
-	STEP_2(context, 2, 9, i + 490);
-	STEP_2(context, 1, 3, i + 491);
-	STEP_2(context, 4, 10, i + 492);
-	STEP_2(context, 1, 5, i + 493);
-	STEP_2(context, 2, 6, i + 494);
-	STEP_2(context, 1, 3, i + 495);
-	STEP_2(context, 11, 21, i + 496);
-	STEP_2(context, 1, 12, i + 497);
-	STEP_2(context, 2, 13, i + 498);
-	STEP_2(context, 1, 3, i + 499);
-	STEP_2(context, 4, 14, i + 500);
-	STEP_2(context, 1, 5, i + 501);
-	STEP_2(context, 2, 6, i + 502);
-	STEP_2(context, 1, 3, i + 503);
-	STEP_2(context, 7, 15, i + 504);
-	STEP_2(context, 1, 8, i + 505);
-	STEP_2(context, 2, 9, i + 506);
-	STEP_2(context, 1, 3, i + 507);
-	STEP_2(context, 4, 10, i + 508);
-	STEP_2(context, 1, 5, i + 509);
-	STEP_2(context, 2, 6, i + 510);
-	STEP_2(context, 1, 3, i + 511);
-
 }
 
 // generated with L = 9
@@ -641,7 +384,7 @@ size_t generic_enum_1x32(int n, const uint32_t * const F_,
 	STEP_0(&context, 0);
 
 	// from now on, hamming weight is >= 1
-	for (int idx_0 = 0; idx_0 < min(n, 9); idx_0++) {
+	for (int idx_0 = 0; idx_0 < min(n, L); idx_0++) {
 
 		// special case when i has hamming weight exactly 1
 		uint32_t w1 = (1 << idx_0);
@@ -653,8 +396,7 @@ size_t generic_enum_1x32(int n, const uint32_t * const F_,
 		// Because of the last step, the current iteration counter is a multiple of 512 plus one
 		// This loop sets it to `rolled_end`, which is a multiple of 512, if possible
 
-		const uint32_t rolled_end = w1 + (1 << min(9, idx_0));
-		for (uint32_t i = 1 + w1; i < rolled_end; i++) {
+		for (uint32_t i = 1 + w1; i < 2 * w1; i++) {
 			int pos = 0;
 			/* k1 == rightmost 1 bit */
 			uint64_t _i = i;
@@ -671,7 +413,7 @@ size_t generic_enum_1x32(int n, const uint32_t * const F_,
 				pos++;
 			}
 			const int k_2 = pos;
-			printf("%08x (rolled)\n", i);
+			//printf("%08x (rolled)\n", i);
 			STEP_2(&context, idx_1(k_1), idx_2(k_1, k_2), i);
 		}
 
@@ -683,7 +425,7 @@ size_t generic_enum_1x32(int n, const uint32_t * const F_,
 	// Here, the number of iterations to perform is (supposedly) sufficiently large
 	// We will therefore unroll the loop 512 times
 
-	for (int idx_0 = 9; idx_0 < n; idx_0++) {	
+	for (int idx_0 = L; idx_0 < n; idx_0++) {	
 
 		// special case when i has hamming weight exactly 1
 		uint32_t w1 = (1 << idx_0);
@@ -696,7 +438,7 @@ size_t generic_enum_1x32(int n, const uint32_t * const F_,
 			return context.n_solutions;
 
 		// unrolled critical section where the hamming weight is >= 2
-		for (uint32_t j = 512; j < (1ull << idx_0); j += 512) {
+		for (uint32_t j = (1 << L); j < (1ull << idx_0); j += (1 << L)) {
 			const uint32_t i = j + w1;
 			int pos = 0;
 			uint32_t _i = i;
@@ -714,7 +456,7 @@ size_t generic_enum_1x32(int n, const uint32_t * const F_,
 			const int k_2 = pos;
 			const int alpha = idx_1(k_1);
 			const int beta = idx_2(k_1, k_2);
-			printf("%08x (unrolled start)\n", i);
+			//printf("%08x (unrolled start)\n", i);
 
 			STEP_2(&context, alpha, beta, i);
 			unrolled_chunk(&context, alpha, i);
