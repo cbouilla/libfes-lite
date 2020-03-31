c3896eb5c542cacae8dbdce73fe85b1f0b96f906
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2020-03-31 11:12:00 +0200

    new "zero" test

---
+++ test/zero.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <err.h>
+#include <stdlib.h>
+
+#include "fes.h"
+#include "rand.h"
+
+/** test that all the kernels find the same solutions */
+
+int n = 16;
+u32 Fq[496];
+int ntest = 0;
+int size;
+
+void test_kernel(int kernel)
+{
+	const char *name = feslite_kernel_name(kernel);
+	printf("# testing kernel [%s]\n", name);
+	if (!feslite_kernel_is_available(kernel)) {
+		printf("ok %d - SKIP kernel [%s] not available\n", ++ntest, name);
+		return;
+	}
+	
+	/* clone original system in all lanes */
+	int m = feslite_kernel_batch_size(kernel);
+	u32 Fl[33 * m];
+	for (int i = 0; i < 33 * m; i++)
+		Fl[i] = 0;
+	
+	/* get all solutions */
+	int size[m];
+	int count = 1 << n;
+	u32 * buffer = malloc(count * m * sizeof(u32));
+	if (buffer == NULL) {
+		printf("Bail out! Cannot allocate solution buffer\n");
+		exit(1);
+	}
+	feslite_kernel_solve(kernel, n, m, Fq, Fl, count, buffer, size);
+	
+	// must reach 1 << n solutions in at least one lane.
+	for (int lane = 0; lane < m; lane++) {
+		if (size[lane] == count) {
+			printf("ok %d - kernel [%s] found the right number of solutions in lane %d\n", ++ntest, name, lane);
+			return;
+		}
+		printf("not ok %d - kernel [%s] never found %d solutions\n", ++ntest, name, count);
+	}
+
+	// todo : sort them, check all present.
+
+	
+}
+
+
+int main()
+{	
+	for (int i = 0; i < 496; i++)
+		Fq[i] = 0;
+	
+	/* go */
+	for (int kernel = 0; kernel < feslite_num_kernels(); kernel++)
+		test_kernel(kernel);
+
+	printf("1..%d\n", ntest);
+	return EXIT_SUCCESS;
+}
