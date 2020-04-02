6206003eb3c01c1161c160a8f938776c6bdf14f3
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2020-04-02 13:44:29 +0200

    better "speed"

--- benchmark/speed.c
+++ benchmark/speed.c
@@ -1,5 +1,7 @@
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <err.h>
 
 #include "feslite.h"
@@ -7,6 +9,12 @@
 	
 /* Measure raw single-thread speed of all kernels in the library */
 int n = 32;
+
+double wtime() {
+	struct timeval ts;
+	gettimeofday(&ts, NULL);
+	return (double)ts.tv_sec + ts.tv_usec / 1E6;
+}
 
 
 void bench_kernel(int kernel)
@@ -17,7 +25,7 @@ void bench_kernel(int kernel)
 		return;
 	}
 	int m = feslite_kernel_batch_size(kernel);
-	printf("kernel %d [%s] : %d lane(s)... ", kernel, name, m);
+	printf("kernel %d [%s] : %d lane(s)...\n", kernel, name, m);
 	fflush(stdout);
 
 	srand48(1337);
@@ -34,12 +42,18 @@ void bench_kernel(int kernel)
 	int count = 256;
 	u32 buffer[count * m];
 	int size[m];
+	double start_wt = wtime();
 	u64 start = Now();
 	feslite_kernel_solve(kernel, n, m, Fq, Fl, count, buffer, size);
 	u64 stop = Now();
-		
-	double rate = ((double) (stop - start)) / m / (1ll << n);
-	printf("---> %.2f cycles/candidate / %.2f candidate/cycle\n", rate, 1/rate);
+	double stop_wt = wtime();
+
+	double cycles = stop - start;
+	double seconds = stop_wt - start_wt;
+	double rate = cycles / m / (1ll << n);
+	printf("\t---> %.2f s\n", stop_wt - start_wt);
+	printf("\t---> %.2f cycles/candidate || %.2f candidate/cycle\n", rate, 1/rate);
+	printf("\t---> 2^%.2f candidate/s\n", log2(m) + n - log2(seconds));
 }
 
 
@@ -49,9 +63,9 @@ int main(int argc, char **argv)
 	int nkernels = feslite_num_kernels();
 
 	if (argc > 1) {
-		int kernel = atoi(argv[1]);
-		if (kernel < 0 || kernel >= nkernels)
-			errx(1, "bad kernel number");
+		int kernel = feslite_kernel_find_by_name(argv[1]);
+		if (kernel < 0)
+			errx(1, "bad kernel name (use ./list)");
 		bench_kernel(kernel);
 	} else {
 		for (int kernel = 0; kernel < nkernels; kernel++)
