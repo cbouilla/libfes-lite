18abd91d8a249c61b18e600c541b554a6d59b728
Charles Bouillaguet <charles.bouillaguet@lip6.fr>
2021-04-11 12:31:58 +0200

    demo program no longer restricted to 64 polynomials. Dropped random systems.

--- README.md
+++ README.md
@@ -22,5 +22,25 @@ Building requires cmake. Building out-of-source is recommended
 The `benchmark/` folder contains code that exercices the library.
 
 - benchmark/correct_use.c : demonstrates how to use the library
-- benchmark/demo.c        : full-blown program that solves a larger system of quadratic equations using all available cores
 - benchmark/speed.c       : benchmarks all available kernels
+- benchmark/demo.c        : full-blown program that solves a larger system of quadratic equations using all available cores
+
+Example:
+```
+# ./demo < examples/random_40_quad.in
+```
+
+## Input format
+
+This code parses polynomial systems modulo 2 in the following format: 
+
++ Lines begining by a `#` are ignored.
++ Spaces, tabs, etc. are ignored.
++ The first non-comment line contains the names of all variables, separated 
+  by commas.  This implicitly numbers them.  The first variable has number 0.
++ Each subsequent lines describe a polynomial.
++ A polynomial is a sum of monomials, separated by `+`
++ A monomial is either `0`, `1`, a single variable, or a product of several 
+  variables separated by `*`.
+
+(It follows that an empty line denote the zero polynomial).
--- benchmark/demo.c
+++ benchmark/demo.c
@@ -1,4 +1,3 @@
-// openmp program that solves a random system of up to 64 eqs in up to 64 vars.
 #include <assert.h>
 #include <sys/time.h>
 #include <inttypes.h>
@@ -8,69 +7,114 @@
 #include <err.h>
 #include <omp.h>
 
+/* this program solves system of (arbitrarily many) quadratic polynomials in 
+   <= MAX_VARIABLES variables. It first loads the system completely, then 
+   converts the first 32 equations into the format libfes-lite expects. 
+   Candidate solutions for the first 32 polynomials are then checked rather 
+   naively against the remaining polynomials.
+
+   If the first 32 polynomials have many solutions, then it would be faster to
+   perform a random linear combination of all the polynomials priori to 
+   enumeration.
+
+   If the first 32 polynomials have more than 65536 solutions, then some 
+   partial solutions will be lost, which may result in full solutions being lost
+   as well. A warning is printed in this case.
+*/
+
 #include "feslite.h"
 #include "parser.h"
 
-	
-static inline int idxq(int i, int j)
+int n;              // #variables
+int n_poly;         // #polynomials in the original system
+
+/********************************* parser interface ***************************/
+
+int idxq(int i, int j)
 {
 	return j * (j - 1) / 2 + i;
 }
 
-u32 eval32(int n, const u32 * Fq, const u32 * Fl, int stride, u32 x)
+struct poly_t {
+	int n_terms;
+	int (*terms)[2];
+};
+
+struct poly_t * poly;
+int capacity;    // size allocated for poly
+
+void prepare_poly(struct poly_t *p)
 {
-	// first expand the values of the variables from `x`
-	u32 v[32];
-	for (int k = 0; k < n; k++) {
-		v[k] = (x & 0x0001) ? 0xffffffff : 0x00000000;
-		x >>= 1;
-	}
+	p->n_terms = 0;
+	int size = idxq(0, n) + n + 1;
+	p->terms = malloc(size * sizeof(*p->terms));
+}
 
-	u32 y = Fl[0];
+void setup(void *opaque, int _n, const char **vars)
+{
+	if (n > 64)
+		errx(1, "This program only handles <= 64 variables.");
+	n = _n;
+	capacity = 64;
+	poly = malloc(capacity * sizeof(*poly));
+	prepare_poly(&poly[0]);
+}
 
-	for (int idx_0 = 0; idx_0 < n; idx_0++) {
-		// computes the contribution of degree-1 terms
-		u32 v_0 = v[idx_0];
-		u32 l = Fl[stride * (1 + idx_0)];   // FIXME : get rid of this multiplication
-		y ^= l & v_0;
+void store_monomial(void *opaque, int line, int column, int degree, const int *variables)
+{
+	if (degree > 2)
+		errx(1, "monomial of degree %d on line %d col %d (max=2)\n", degree, line, column);
 
-		for (int idx_1 = 0; idx_1 < idx_0; idx_1++) {
-			// computes the contribution of degree-2 terms
-			u32 v_1 = v_0 & v[idx_1];
-			u32 q = Fq[idxq(idx_1, idx_0)];
-			y ^= q & v_1;
+	// reduce squares
+	if (degree == 2 && variables[0] == variables[1])
+		degree = 1;
+
+	struct poly_t *p = &poly[n_poly];
+	int i = p->n_terms;
+	int u = -1;
+	int v = -1;
+	if (degree == 1) {
+		u = variables[0];
+	} else if (degree == 2) {
+		// order variables
+		if (variables[0] < variables[1]) {
+			u = variables[0];
+			v = variables[1];
+		} else {
+			u = variables[1];
+			v = variables[0];
 		}
 	}
-	return y;
+	p->terms[i][0] = u;
+	p->terms[i][1] = v;
+	p->n_terms = i + 1;
 }
 
-u64 eval64(int n, const u64 * Q, const u64 * L, u64 x)
+void store_polynomial(void *opaque, int line)
 {
-	u64 v[64];
-	for (int k = 0; k < n; k++) {
-		v[k] = (x & 1) ? 0xffffffffffffffffull : 0;
-		x >>= 1ull;
-	}
-	assert(x == 0);
-	u64 y = L[0];
+	if (poly[n_poly].n_terms == 0)
+		return;
 
-	for (int i = 0; i < n; i++) {
-		// computes the contribution of degree-1 terms
-		y ^= L[1 + i] & v[i];
-		for (int j = 0; j < i; j++)
-			y ^= Q[idxq(j, i)] & v[i] & v[j];
+	n_poly += 1;
+	if (n_poly == capacity) {
+		capacity = 1 + 2 * capacity;
+		poly = realloc(poly, capacity * sizeof(*poly)); 
 	}
-	return y;
+	prepare_poly(&poly[n_poly]);
 }
 
-/* initial system on <= 64 variables */
-u64 Fq_start[2016];
-u64 Fl_start[65];
+/***************************** parallel solving code **************************/
 
-/********************************* parallel solving code **************************/
+int h;              // number of specialized variables
+int m;              // #lanes of the solver kernel
+int n_poly_feslite; // #polynomials solved for using libfes-lite
+u64 partial_solutions, total_solutions;
+bool warned_lost;
 
-// 32-bit truncated version
+/* initial system on <= 64 variables, truncated to 32 polynomials */
 u32 Fq[2016];
+u32 Fl[65];
+
 #define MAX_BATCH_SIZE 64
 
 struct bundle_t {
@@ -79,9 +123,6 @@ struct bundle_t {
 	u32 Fl[];
 };
 
-int n;  // #variables
-int h;  // number of specialized variables
-int m;  // #lanes of the solver kernel
 struct bundle_t *current_bundle;
 int in_flight, created, solved;
 
@@ -98,50 +139,78 @@ void fresh_bundle()
 
 void print_solution(u64 x)
 {
-        printf("\nsolution found : (");
-        for (int i = 0; i < n; i++) {
-                printf("%01" PRId64, x & 1);
-                x >>= 1;
-                if (i != n-1)
-                        printf(", ");
+	printf("solution found : (");
+	for (int i = 0; i < n; i++) {
+		printf("%01" PRId64, x & 1);
+		x >>= 1;
+		if (i != n-1)
+			printf(", ");
+	}
+	printf(")\n");
+}
+
+// evaluate poly[i] on x
+bool check_poly(int i, u64 x)
+{
+        bool y = false;
+        struct poly_t *p = &poly[i];
+        for (int j = 0; j < p->n_terms; j++) {
+        	int u = p->terms[j][0];
+		int v = p->terms[j][1];
+		if (u == -1) {
+			y ^= 1;
+		} else if (v == -1) {
+			y ^= (x >> u) & 1;
+		} else {
+			y ^= (x >> u) & (x >> v) & 1;
+		}
         }
-        printf(")\n");
+//        fprintf(stderr, "checking %" PRIx64 " against poly[%d], with %d terms --> %d\n", x, i, p->n_terms, y);
+        return y;
 }
 
 void process_bundle(struct bundle_t *ready_bundle)
 {
 	/* solve ready bundle */
-	int count = 256;
+	int count = 65536;
 	u32 buffer[count * m];
 	int size[m];
 	feslite_solve(n - h, m, Fq, ready_bundle->Fl, count, buffer, size);
 
-	// check against remaining equations, print
+	// check partial solutions against remaining equations, print full solutions
 	for (int i = 0; i < m; i++) {
 		if (size[i] == count)
-			warn("Possibly lost solution\n");
+			#pragma omp critical
+			{
+				if (!warned_lost)
+					warnx("Possibly lost solutions");
+				warned_lost = true;
+			}
+		#pragma omp atomic	
+		partial_solutions += size[i];
 		for (int j = 0; j < size[i]; j++) {
 			u32 x = buffer[count*i + j];
-			u32 y = eval32(n - h, Fq, ready_bundle->Fl + i, m, x);
-			// printf("partial %08x\n", x);
-			assert(y == 0);
 			u64 p = ready_bundle->prefixes[i];
 			u64 u = x ^ (p << (n - h));
-			u64 v = eval64(n, Fq_start, Fl_start, u);
-			if (v == 0)
+			// check partial solution against other polynomials
+			bool bad = false;
+			for (int j = n_poly_feslite; j < n_poly; j++)
+				bad |= check_poly(j, u);
+			if (!bad) {
+				#pragma omp critical
 				print_solution(u);
+				#pragma omp atomic
+				total_solutions += 1;
+			}
 		}
 	}
 
 	/* free ready bundle */
 	free(ready_bundle);
-
 	#pragma omp atomic
 	solved += m;
-
 	#pragma omp atomic
 	in_flight--;
-
 	#pragma omp critical
 	{
 		printf("\rcreated: %d\t Solved: %d\t In-flight: %d    ", created, solved, in_flight);
@@ -156,10 +225,8 @@ void push(const u32 * Fl, u32 prefix)
 
 	/* copy to current bundle */
 	current_bundle->prefixes[current_bundle->i] = prefix;
-	for (int j = 0; j < n - h + 1; j++) {
+	for (int j = 0; j < n - h + 1; j++)
 		current_bundle->Fl[m *j + current_bundle->i] = Fl[j];
-		// printf("Fl[%2d] = %08x\n", j, Fl[j]);
-	}
 	current_bundle->i += 1;
 
 	/* bundle full? */
@@ -194,96 +261,12 @@ void specialize(int fixed, const u32 * Fl, u32 prefix)
 	specialize(fixed + 1, Fl_, (prefix << 1) ^ 1);
 }
 
-/************************************* random systems **************************************/
-
-void usage()
-{
-	printf("./demo N                  generate and solve a random quadratic system on N var");
-	printf("./demo < equations.txt    parse and solve the quadratic system given in the file");
-}
-
-
-u64 rand64()
-{
-	return ((u64) lrand48()) ^ (((u64) lrand48()) << 32ull);
-}
-
-void setup_random()
-{
-	printf("Generating random system of %d variables / %d quadratic polynomials\n", n, n);
-	srand48(1337);
-	
-	/* initalize a random system */	
-	int N = idxq(0, n);
-	for (int i = 0; i < N; i++)
-		Fq_start[i] = rand64();
-	for (int i = 0; i < n+1; i++)
-		Fl_start[i] = rand64();
-	u64 x = rand64() & ((1ull << n) - 1);   /* designated solution */
-	Fl_start[0] ^= eval64(n, Fq_start, Fl_start, x);
-	assert(0 == eval64(n, Fq_start, Fl_start, x));
-	printf("Planted: %016" PRIx64 "\n", x);	
-}
-
-/**** parser interface *****/
-
-int n_poly;      // #polynomials
-bool nonempty;   // current polynomial empty?
-
-void store_variables(void *opaque, int _n, const char **_vars)
-{
-        n = _n;
-}
-
-void store_monomial(void *opaque, int line, int column, int degree, const int *variables)
-{
-        if (n_poly >= 64)
-                return;
-        if (degree > 2)
-                errx(1, "monomial of degree %d on line %d col %d (max=2)\n", degree, line, column);
-        u64 mask = (1ull << n_poly);
-        if (degree == 0)
-        	Fl_start[0] ^= mask;
-        if (degree == 1)
-        	Fl_start[1 + variables[0]] ^= mask;
-        if (degree == 2) {
-        	int u = variables[0];
-        	int v = variables[1];
-        	if (u == v)
-        		errx(1, "squares not allowed (line %d, col %d)\n", line, column);
-        	if (u < v)
-        		Fq_start[idxq(u, v)] ^= mask;
-        	else
-        		Fq_start[idxq(v, u)] ^= mask;
-        }
-        nonempty = true;
-}
-
-void store_polynomial(void *opaque, int line)
-{
-        if (nonempty == false)
-                return;
-        if (n_poly >= 64)
-                warnx("Ignoring polynomial on line %d\n", line);
-        n_poly += 1;
-        nonempty = false;
-}
-
-void finalize(void *opaque)
-{
-        printf("Read %d polynomials in %d variables\n", n_poly, n);
-}
-
-/*********************************************** go ******************************************/
+/******************************** go ******************************************/
 
 int main(int argc, char **argv)
 {
-	if (argc > 1) {
-		n = atoi(argv[1]);
-		setup_random();
-	} else {
-		parser(stdin, NULL, &store_variables, &store_monomial, &store_polynomial, &finalize);
-	}
+	parser(stdin, NULL, &setup, &store_monomial, &store_polynomial, NULL);
+	fprintf(stderr, "Read %d polynomials in %d variables\n", n_poly, n);
 
 	int kernel = feslite_default_kernel();
 	int min_n = feslite_kernel_min_variables(kernel);
@@ -298,21 +281,29 @@ int main(int argc, char **argv)
 		h = n - 32;
 	while ((1 << h) < m)
 		h += 1;
-	printf("Specializing %d variables\n", h);
-	printf("Using kernel %s on %d variables, using %d lane(s)...\n", name, n - h, m);
-	
-	/* create the truncated 32 bits version in Fl / Fq */
-	int N = idxq(0, n);
-	u32 Fl[65];
-	for (int i = 0; i < N; i++) {
-		Fq[i] = Fq_start[i] & 0xffffffff;
-		// printf("Fq[%3d] = %08x\n", i, Fq[i]);
+	n_poly_feslite = n_poly;
+	if (n_poly_feslite > 32)
+		n_poly_feslite = 32;
+	fprintf(stderr, "Specializing %d variables\n", h);
+	fprintf(stderr, "Using kernel %s on %d variables, using %d lane(s)...\n", name, n - h, m);
+	fprintf(stderr, "Enumerating %d polynomials, checking partial solutions against %d remaining polynomials\n", n_poly_feslite, n_poly - n_poly_feslite);
+
+	/* create the truncated 32 bits version in Fl / Fq for enumeration */
+	for (int i = 0; i < n_poly_feslite; i++) {
+		struct poly_t *p = &poly[i];
+		for (int j = 0; j < p->n_terms; j++) {
+			int x = p->terms[j][0];
+			int y = p->terms[j][1];
+			if (x == -1) {
+				Fl[0] += 1 << i;
+			} else if (y == -1) {
+				Fl[1 + x] += 1 << i;
+			} else {
+				Fq[idxq(x, y)] += 1 << i;
+			}
+		}
 	}
-	for (int i = 0; i < n+1; i++) {
-		Fl[i] = Fl_start[i] & 0xffffffff;
-		// printf("Fl[%2d] = %08x\n", i, Fl[i]);
-	}
-	
+
 	/* solve */
 	double start_wt = omp_get_wtime();
 	fresh_bundle();
@@ -329,8 +320,10 @@ int main(int argc, char **argv)
 
 	double seconds = stop_wt - start_wt;
 	double rate = n - log2(seconds);
-	printf("\n");
-	printf("\t---> Finished in %.2f s\n", seconds);
-	printf("\t---> 2^%.2f candidate/s\n", rate);
+	fprintf(stderr, "\n");
+	fprintf(stderr, "\t---> %" PRId64 " partial solution(s) checked\n", partial_solutions);
+	fprintf(stderr, "\t---> %" PRId64 " solution(s) found\n", total_solutions);
+	fprintf(stderr, "\t---> Finished in %.2f s\n", seconds);
+	fprintf(stderr, "\t---> 2^%.2f candidate/s\n", rate);
 	return EXIT_SUCCESS;
 }
